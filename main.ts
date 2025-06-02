abddb75e2be8cc90fab4cd69935a3e3952ca461b
andreas riedmann <andreas.riedmann@htldornbirn.at>
2025-06-02 15:19:40 +0200

    try

--- main.ts
+++ main.ts
@@ -12,6 +12,7 @@ interface Person {
 let people: Person[] = [
   { id: 1, firstName: "Alice", lastName: "Johnson", avatar: "https://i.pravatar.cc/150?img=1" },
   { id: 2, firstName: "Bob", lastName: "Smith", avatar: "https://i.pravatar.cc/150?img=2" },
+  { id: 3, firstName: "Charlie", lastName: "Brown", avatar: "URL_ADDRESS.pravatar.cc/150?"}
 ];
 
 const router = new Router();
